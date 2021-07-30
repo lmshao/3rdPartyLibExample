e2ab6c96a772e5e993ee23729e08b937f2dd66c9
Liming SHAO <lmshao@163.com>
2021-07-30 15:58:12 +0800

    update

--- JWT-ABC/main.cpp
+++ JWT-ABC/main.cpp
@@ -16,11 +16,19 @@ int main()
                           .set_payload_claim("sample", jwt::claim(std::string("abc")))  // customized
                           .sign(jwt::algorithm::hs256{ "123456" });                     // alg
 
+    // std::string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJ2aWNlIjoiTElNSU5HLURFViIsInNlcnZpY2VLZXkiOiI4ZDdjYTlmYWVmMzdkM2ZkIiwiZXhwaXJlQXQiOjE1ODY3NDczMTZ9.Qw5vft+SfgDmnIN2/vG79QEQQLFGC9LIipY4AeVzceU=";
     std::cout << "token: " << token << std::endl;
 
     sleep(2);
     // Decode a token
-    auto decoded = jwt::decode(token);
+    std::unique_ptr<jwt::decoded_jwt> decoded;
+    try {
+        auto de = jwt::decode(token);
+        decoded.reset(new jwt::decoded_jwt(de));
+    } catch (std::exception &e) {
+        std::cout << "jwt::decode: " << e.what() << std::endl;
+        return -1;
+    }
 
     // Create a verifier
     auto verifier = jwt::verify()
@@ -28,9 +36,9 @@ int main()
                       .with_issuer("server")                               // issuer
                       .with_subject("topic");                              // topic
     try {
-        verifier.verify(decoded);
+        verifier.verify(*decoded);
         std::cout << "verify successfully!" << std::endl;
-    } catch (std::runtime_error &e) {
+    } catch (std::exception &e) {
         std::cout << "verify error: " << e.what() << std::endl;
         std::string str(e.what());
         if (str.find("token expired") != std::string::npos) {
@@ -39,13 +47,13 @@ int main()
     }
 
     // Get all payload
-    for (auto &e : decoded.get_payload_claims()) {
+    for (auto &e : decoded->get_payload_claims()) {
         std::cout << e.first << " = " << e.second.to_json() << std::endl;
     }
 
     // need to catch exception
     try {
-        std::string sample = decoded.get_payload_claim("sample").as_string();
+        std::string sample = decoded->get_payload_claim("sample").as_string();
         std::cout << "sample = " << sample << std::endl;
     } catch (std::runtime_error &e) {
         std::cout << e.what() << std::endl;
